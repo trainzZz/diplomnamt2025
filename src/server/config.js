f2442730523cc1dfeea24334a759f86391f53475
Sergey Luzin <starry.cs01@gmail.com>
2025-06-15 21:43:06 +0300

    sec

--- src/server/config.js
+++ src/server/config.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN || '7452171339:AAHnYW3-AFgvZu_DtPg1ToExeGhkMmoEfjQ',
-  SERVER_PORT: process.env.PORT || 3001
+  SERVER_PORT: 3003
 }; 
